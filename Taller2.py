81d344ad65e49d524cbbc1a88dd1f04a4661c9a5
Juan Quinones <jsqo4320@gmail.com>
2026-08-17 18:43:41 -0500

    Modificacion en la linea 14: Cambia de Selling_Price a Ex_Showroom_Price Nueva linea 16: Correlacion entre Ex y Selling

--- Taller2.py
+++ Taller2.py
@@ -11,4 +11,6 @@ print(df['Brand'].value_counts())
 
 print("Correlación entre Year y KM_Driven:", df['Year'].corr(df['KM_Driven']))
 print("Correlación entre KM_Driven y Price:", df['KM_Driven'].corr(df['Selling_Price']))
-print("Correlación entre Year y Selling_Price:", df['Year'].corr(df['Selling_Price']))
+print("Correlación entre Year y Ex_Showroom_Price:", df['Year'].corr(df['Ex_Showroom_Price']))
+
+print("Correlación entre Ex_Showroom_Price y Selling_Price:", df['Ex_Showroom_Price'].corr(df['Selling_Price']))
